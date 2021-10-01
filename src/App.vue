252293d11d35712f7df5dc2b101e1c6a84be052f
Beatriz Silva Souza <msfemacell@gmail.com>
2021-10-01 20:22:53 -0300

    criação do projeto do compeonato brasileiro e finalização da tela Inicio

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <AppTopBar />
+    <v-main>
+      <Inicio />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Inicio from "./components/Inicio.vue";
+import AppTopBar from "./components/shared/AppTopBar.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  name: "App",
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: { Inicio, AppTopBar },
+};
+</script>
---
+++ src/components/Inicio.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container>
+    <h2 class="text-h5 text-center mb-3 mt-5">Brasileiração Serie A</h2>
+
+    <v-img
+      src="https://diariodonordeste.verdesmares.com.br/image/contentid/policy:1.2974821:1596806515/Serie-A.png"
+      class="inicio-imagem mb-4"
+    />
+
+    <p class="paragrafo">
+      A Série A do Campeonato Brasileiro de Futebol de 2020, oficialmente
+      Brasileirão Assaí – Série A 2020 por motivos de patrocínio, é a 64.ª
+      edição da principal divisão do futebol brasileiro. A disputa tem o mesmo
+      regulamento dos anos anteriores, quando foi implementado o sistema de
+      pontos corridos.
+    </p>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Inicio",
+};
+</script>
+
+<style scoped>
+.inicio-imagem {
+  border-radius: 3px;
+}
+.paragrafo {
+  text-align: justify;
+}
+</style>
